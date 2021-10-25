4758259747beaaf6a8ba5e7de2c9b50070c1ca9d
Umair Abbas <umairabbass786@gmail.com>
2021-10-25 19:55:55 +0500

    some changes

--- forms/contact.php
+++ forms/contact.php
@@ -1,16 +1,17 @@
 <?php
-$to = "aamriabbas@gmail.com";
+$to = "mute.dispatchers.int@gmail.com";
 $name = $_POST['name'];
+$phone = $_POST['phone'];
 $subject = $_POST['subject'];
 $from = $_POST['email'];
 $txt = $_POST['message'];
-$msg = "Name = $name\r\n" . "Sender = $from\r\n" . "Message = $txt\r\n";
+$msg = "Name = $name\r\n" . "Phone Number = $phone\r\n" . "Sender = $from\r\n" . "Message = $txt\r\n";
 $headers = "From: info@muteint.com";
 
 if(mail($to,$subject,$msg,$headers)){
     echo "Your message has been sent. Thank you!";
 }
 else{
-    echo "Error!";
+    echo "Unable to Send Mail";
 }
 ?>
